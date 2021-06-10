2355a013729bad09cc8b56cb4e0c04c16c463444
richardanyalai <gtaricsi@gmail.com>
2021-06-10 08:16:40 +0200

    Refactored service functions to arrow functions

--- src/service.ts
+++ src/service.ts
@@ -7,22 +7,22 @@ import config from "./config";
 const API_BASE_URL = config.hubUrl;
 const PLATFORM = "telegram";
 
-async function onChatStart(ctx: any) {
+const onChatStart = (ctx: any): void => {
   if (ctx.message.chat.id > 0)
     // TODO: check whether the user is in the database
-    await ctx.replyWithMarkdown(
+    ctx.replyWithMarkdown(
       [
         "I'm sorry, I couldn't find you in the database.",
         "Make sure to register [here](https://agora.space/)."
       ].join("\n")
     );
-}
+};
 
-async function onUserJoined(
+const onUserJoined = (
   refId: string,
   idFromPlatform: string,
   sender: string
-) {
+): void => {
   axios
     .post(`${API_BASE_URL}/user/joined`, {
       refId,
@@ -32,13 +32,13 @@ async function onUserJoined(
     })
     .then((res) => logger.debug(JSON.stringify(res.data)))
     .catch(logger.error);
-}
+};
 
-async function onUserLeft(ctx: any) {
+const onUserLeft = (ctx: any): void => {
   ctx.reply(`Bye, ${ctx.message.left_chat_member.first_name} 😢`);
-}
+};
 
-async function onUserRemoved(idFromPlatform: string, sender: string) {
+const onUserRemoved = (idFromPlatform: string, sender: string): void => {
   axios
     .post(`${API_BASE_URL}/user/removed`, {
       idFromPlatform,
@@ -47,6 +47,6 @@ async function onUserRemoved(idFromPlatform: string, sender: string) {
     })
     .then((res) => logger.debug(JSON.stringify(res.data)))
     .catch(logger.error);
-}
+};
 
 export { onChatStart, onUserJoined, onUserLeft, onUserRemoved };
