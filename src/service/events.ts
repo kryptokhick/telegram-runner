82a02a8065045d2ef3c1e614f38a9223ebe05b17
dezsim <dezsimartin@gmail.com>
2021-07-19 13:33:36 +0200

    reply typo fix, removed platformuserId from reply

--- src/service/events.ts
+++ src/service/events.ts
@@ -60,7 +60,6 @@ const onChatStart = (ctx: any): void => {
 
           if (invites.length) {
             ctx.replyWithMarkdown(
-              platformUserId,
               "You have 15 minutes to join these groups before the invite " +
                 "links expire:",
               Markup.inlineKeyboard(
