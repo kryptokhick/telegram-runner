d6d85d22c8b526e6bf3a543a24cacab30a3f1df0
DevidAbt <abt.devid@gmail.com>
2022-01-06 23:59:46 +0100

    Changed login script

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -1,5 +1,23 @@
-import { sendLoginCode, signIn } from "../service/actions";
+import config from "../config";
+import mtprotoApi from "../mtproto";
 import logger from "../utils/logger";
+
+const sendLoginCode = async () =>
+  mtprotoApi.call("auth.sendCode", {
+    phone_number: config.phoneNumber,
+    api_id: config.mtproto.apiId,
+    api_hash: config.mtproto.apiHash,
+    settings: {
+      _: "codeSettings"
+    }
+  });
+
+const signIn = async (phoneCode: string, phoneCodeHash: string) =>
+  mtprotoApi.call("auth.signIn", {
+    phone_number: config.phoneNumber,
+    phone_code: phoneCode,
+    phone_code_hash: phoneCodeHash
+  });
 
 (async () => {
   const result = await sendLoginCode();
